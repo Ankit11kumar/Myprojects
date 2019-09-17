35c0e0aef4c271efdf4f7bdfae61e1b41e03bcdb
Ankit Kumar <35497605+Ankit11kumar@users.noreply.github.com>
2019-09-17 19:16:04 +0530

    Update Live_color_tracking.py

--- Live_color_tracking.py
+++ Live_color_tracking.py
@@ -16,26 +16,18 @@ while(1):
     if(M["m00"]!=0):
         cx = int(M["m10"] / M["m00"])
         cy = int(M["m01"] / M["m00"])
-    #mouse.position = (cx,cy)
+    mouse.position = (cx,cy)
     
     image,contours,hierarchy = cv2.findContours(k,1,2)  # contours means white patches..will return array of contours.
     c=0
     for s in range(0,len(contours)):
         area = cv2.contourArea(contours[s])
         if(area>500): 
-            c=c+1
-            cv2.drawContours(i,contours[s],-1,(0,255,255),3)
-            cv2.drawContours(k,contours[s],-1,(0,255,255),3)
-            #mouse.press(Button.left)
-            #mouse.release(Button.left)
-            #mouse.click(Button.left,2)
+            mouse.press(Button.left)
+        if(area>800):
+            mouse.release(Button.left)
             
-    print('no. of images detected--',c)
-    #cv2.imshow('difference',d)
-    cv2.imshow('cam',i)
-    cv2.imshow('final',k)
-
-    k=cv2.waitKey(2)
+    k=cv2.waitKey(1)
     if(k==ord('q')):
         cv2.destroyAllWindows()
         v.release()
